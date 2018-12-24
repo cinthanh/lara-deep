2d56e56de3b003b5ec2d86c18e42b0c41ab08cf6
JsCore <thanhfgit@gmail.com>
2018-12-24 10:36:22 -0500

    make:auth admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
