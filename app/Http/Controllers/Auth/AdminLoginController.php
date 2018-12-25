69dcb2ff8867b39815664494b7a7561ab46a9363
JsCore <thanhfgit@gmail.com>
2018-12-25 11:46:13 -0500

    admin login successful

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginController extends Controller
 {
@@ -18,9 +19,28 @@ class AdminLoginController extends Controller
         return view('auth.admin-login');
     }
 
-    public function login()
+    public function login(Request $request)
     {
-        return true;
+        // validate form login for admin
+        $this->validate($request, [
+            'email'     => 'required|email|max:50',
+            'password'  => 'required|min:5'
+        ]);
+        // Attempt to log the user in
+        if ( Auth::guard('admin')->attempt(
+            [
+                'email' => $request->email,
+                'password' => $request->password
+            ], $request->remember)
+        ) {
+            // if successful: redirect to dashboard
+            return redirect()->intended('admin.dashboard');
+        }
+        return redirect()->back()->withInput($request->only('email','remember'));
+
+        // if unsuccessful redirect back with error
+
+
     }
 
 }
